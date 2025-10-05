ecfb0f204cf68cc9b6034ed2968b608480cd2aee
@L-it <workspace@l-it.am>
2025-10-05 17:53:08 +0400

    updated ProductOutOfStockDTO

--- src/DTO/ProductOutOfStockDTO.php
+++ src/DTO/ProductOutOfStockDTO.php
@@ -8,12 +8,14 @@ use Symfony\Component\Uid\Uuid;
 final class ProductOutOfStockDTO implements PublishedDTOInterface
 {
     public Uuid $orderId;
+    public Uuid $productId;
     public int $quantity;
 
     public static function init(array $orderReservationData): self
     {
         $dto = new self();
         $dto->orderId = $orderReservationData['orderId'];
+        $dto->productId = $orderReservationData['productId'];
         $dto->quantity = $orderReservationData['quantity'];
 
         return $dto;
